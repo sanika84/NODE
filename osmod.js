5a88a4b54ea55473314ab1a75fcfcc4a3044cccc
sanika84 <138477952+sanika84@users.noreply.github.com>
2023-07-15 19:23:00 +0530

    Add files via upload

---
+++ osmod.js
@@ -0,0 +1,15 @@
+const os = require("os");
+
+// console.log(os.arch());
+
+// const freeMemory = Os . freemem();
+// // console.log(freeMemory);
+// console.log ( '${freeMemory/ 1024 / 1024 / 1024}');
+
+// const totalmemory = os.totalmem();
+// console.log( '${totalMemory / 1024 / 1024 / 1024}');
+console.log(os.arch());
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.tmpdir());
+console.log(os.type());
