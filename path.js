ea6e2aac541e21a08e8abae71e1ba80a909cbaea
sanika84 <138477952+sanika84@users.noreply.github.com>
2023-07-15 19:25:43 +0530

    Add files via upload

---
+++ path.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+// console.log(path.dirname("E:/nodejsYoutube/PathModule/path.js"));
+// console.log(path.extname("E:/nodejsYoutube/PathModule/path.js"));
+// console.log(path.basename("E:/nodejsYoutube/PathModule/path.js"));
+
+// const myPath = path.parse("E:/nodejsYoutube/PathModule/path.js");
+// console.log(myPath.name);
+console.log(myPath.root);
+
+
+
+
